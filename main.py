af62ee92668c48a12ede2ea2a31aa575ba4ef9f1
hidriali <159208504+hidriali@users.noreply.github.com>
2024-03-25 22:57:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,108 @@
+#(a)
+#a=1, b=2 ,c=5
+#while a < c :
+# a=a+1
+#b = b+c (endwhile)
+ # output (a,b,c)
+a = 1
+b = 2
+c = 5
+while a < c:
+  a +=1
+  b +=c
+print(f" a= {a}, b= {b}, c= {c}")
+
+
+#(b)
+#d = 4, e = 6, f = 7
+#while d > f
+#d=d+1 , e = e-1 
+#endwhile
+#output (d,e,f)
+d = 4
+e = 6
+f = 7
+while d > f:
+  d +=1
+  e -=1 
+print(f" d= {d}, e= {e}, f= {f}")
+
+
+#(c)
+#g = 4, h = 6
+# while g < h
+#g=g+1 
+# endwhile 
+# output (g,h)
+g = 4
+h = 6
+while g < h:
+  g +=1
+print(f" g= {g}, h= {h}")
+
+
+#(d)
+#j = 2, k = 5 ,n = 9
+#while j < k
+ # m = 6
+#while m < n:
+#output ("goodbye)")
+# m=m+1
+#endwhile
+#j=j+1
+#endwhile 
+j = 2
+k = 5
+n = 9
+while j < k:
+  m=6
+  while m < n:
+    print("goodbye")
+    m +=1
+    break
+  j=j+1
+
+
+#(e)
+#j= 2, k=5, m=6 ,n =9
+#while j < k:
+ #while m < n: 
+  #output "he;;o"
+  #m=m+1
+  #endwhile
+  #j=j+1
+  #endwhile 
+j= 2
+k=5
+m=6
+n =9
+while j < k:
+  while m < n:
+    print("hello")
+    m +=1
+    break
+  j=j+1
+
+
+
+#(f)
+#p=2, q=4, r=1
+#while p<q
+#output"adios"
+#r=1
+#whike r > q
+#output"adios
+#r=r+1
+#endwhile 
+#p= p+1
+#endwhile
+p=2
+q=4
+r=1
+while p<q:
+  while r < q:
+    print("adios")
+    r=r+1
+    break
+p= p+1
+
